ed5b9c9ffa2b4effeb82d31ccb56499edadbc18c
PatriciaAbreu <PatriciaAbreu@MacBook-Pro-de-Patricia.local>
2015-08-02 19:02:42 -0300

    Botão de play e pause

--- testeVideo/testeVideo/ViewController.swift
+++ testeVideo/testeVideo/ViewController.swift
@@ -18,28 +18,47 @@ class ViewController: UIViewController {
 
     var movie: MPMoviePlayerController?
     
+    @IBOutlet weak var videoView: UIView!
+    
+    @IBOutlet weak var btnPlayEPause: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        playMovie()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func playMovie(){
         
         if let path = NSBundle.mainBundle().pathForResource("teste", ofType: "mov"), let url = NSURL.fileURLWithPath(path), movie = MPMoviePlayerController(contentURL: url) {
+            
             self.movie = movie
-            movie.view.frame = self.view.bounds
-            movie.prepareToPlay()
+            movie.view.frame = videoView.bounds
+            movie.view.layer.position.x = 380
+            movie.view.layer.position.y = 250
+//            movie.prepareToPlay()
             movie.scalingMode = .AspectFill
             self.view.addSubview(movie.view)
+            
         }else {
             debugPrintln("Video não encontrado")
         }
     }
+    @IBAction func btnPlay(sender: AnyObject) {
+        if btnPlayEPause.titleLabel?.text == "PLAY" {
+            playMovie()
+            movie?.play()
+            btnPlayEPause.setTitle("PAUSE", forState: UIControlState())
+        }else {
+            movie?.pause()
+            btnPlayEPause.setTitle("PLAY", forState: UIControlState())
+        }
+        
+    }
+    
 }
 
